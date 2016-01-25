f7f5515b0088108abd9d609b25bfd1067665d2eb
Peter Mosmans <support@go-forward.net>
2016-01-25 14:26:01 +1000

    Added arguments for binaries

--- docbuilder.py
+++ docbuilder.py
@@ -44,33 +44,49 @@ the Free Software Foundation, either version 3 of the License, or
                         help='overwrite output file if it already exists')
     parser.add_argument('--fop-config', action='store',
                         default='/usr/local/bin/fop-1.1/conf/rosfop.xconf',
-                        help='fop configuration file (default /usr/local/bin/fop-1.1/conf/rosfop.xconf')
-    parser.add_argument('-f', '--fop', action='store', default='./Report/target/report.fo',
-                        help='intermediate fop output file (default: ./Report/target/report.fo)')
-    parser.add_argument('-i', '--input', action='store', default='./Report/source/report.xml',
-                        help='input file (default: ./Report/source/report.xml)')
-    parser.add_argument('-x', '--xslt', action='store', default='./Report/xslt/content.xsl',
+                        help="""fop configuration file (default
+                        /usr/local/bin/fop-1.1/conf/rosfop.xconf""")
+    parser.add_argument('-f', '--fop', action='store',
+                        default='./Report/target/report.fo',
+                        help="""intermediate fop output file (default:
+                        ./Report/target/report.fo)""")
+    parser.add_argument('--fop-binary', action='store',
+                        default='/usr/local/bin/fop-1.1/fop',
+                        help='fop binary (default /usr/local/bin/fop-1.1/fop')
+    parser.add_argument('-i', '--input', action='store',
+                        default='./Report/source/report.xml',
+                        help="""input file (default:
+                        ./Report/source/report.xml)""")
+    parser.add_argument('--saxon', action='store',
+                        default='/usr/local/bin/saxon/saxon9he.jar',
+                        help="""saxon JAR file (default
+                        /usr/local/bin/saxon/saxon9he.jar)""")
+    parser.add_argument('-x', '--xslt', action='store',
+                        default='./Report/xslt/content.xsl',
                         help='input file (default: ./Report/xslt/auto.xsl)')
-    parser.add_argument('-o', '--output', action='store', default='./Report/target/latest.pdf',
-                        help='output file name (default: ./Report/target/latest.pdf')
+    parser.add_argument('-o', '--output', action='store',
+                        default='./Report/target/latest.pdf',
+                        help="""output file name (default:
+                        ./Report/target/latest.pdf""")
     parser.add_argument('-v', '--verbose', action='store_true',
                         help='increase output verbosity')
     parser.add_argument('-w', '--warnings', action='store_true',
                         help='show warnings')
     args = parser.parse_args()
-    if args.verbose: #pylint: disable=missing-docstring
-        def verboseprint(*args):
+    if args.verbose:
+        def verboseprint(*args):  # pylint: disable=missing-docstring
             for arg in args:
                 print(arg, end="")
             print()
-        def verboseerror(*args):
+
+        def verboseerror(*args):  # pylint: disable=missing-docstring
             for arg in args:
                 print(arg, end="", file=sys.stderr)
             print(file=sys.stderr)
     else:
         verboseprint = lambda *a: None
         verboseerror = lambda *a: None
-    return args.input, args.xslt, args.fop, args.output, args.fop_config, args.clobber
+    return vars(parser.parse_args())
 
 
 def print_output(stdout, stderr):
@@ -99,12 +115,13 @@ def change_tag(fop):
             print('[+] Embedding git version information into document')
 
 
-def to_fo(binary, input_file, xsl_file, fop):
+def to_fo(options):
     """
     Creates a fo output file based on a XML file.
     """
-    cmd = ['java', '-jar', binary, '-s:' + input_file,
-           '-xsl:' + xsl_file, '-o:' + fop, '-xi']
+    cmd = ['java', '-jar', options['saxon'],
+           '-s:' + options['input'], '-xsl:' + options['xslt'],
+           '-o:' + options['fop'], '-xi']
     process = subprocess.Popen(cmd, stdout=PIPE, stderr=PIPE)
     stdout, stderr = process.communicate()
     print_output(stdout, stderr)
@@ -112,15 +129,16 @@ def to_fo(binary, input_file, xsl_file, fop):
         print_exit('[-] Error creating fo file from XML input',
                    process.returncode)
     else:
-        change_tag(fop)
+        change_tag(options['fop'])
     return True
 
 
-def to_PDF(binary, config, fop, output_file):
+def to_PDF(options):
     """
     Creates a PDF file based on a fo file.
     """
-    cmd = [binary, '-c', config, fop, output_file]
+    cmd = [options['fop_binary'], '-c', options['config'], options['fop'],
+           options['output']]
     try:
         process = subprocess.Popen(cmd, stdout=PIPE, stderr=PIPE)
         stdout, stderr = process.communicate()
@@ -146,27 +164,26 @@ def main():
     global verboseerror
     global verboseprint
     result = -1
-    fop_binary = '/usr/local/bin/fop-1.1/fop'
-    saxon_binary = '/usr/local/bin/saxon/saxon9he.jar'
-    input_file, xsl_file, fop_output, output_file, fop_config, clobber = parse_arguments()
-    if not os.path.isfile(input_file):
-        print_exit('[-] Cannot find input file {0}'.format(input_file), result)
+    options = parse_arguments()
+    if not os.path.isfile(options['input']):
+        print_exit('[-] Cannot find input file {0}'.
+                   format(options['input']), result)
     try:
-        if os.path.isfile(output_file):
-            if not clobber:
+        if os.path.isfile(options['output']):
+            if not options['clobber']:
                 print_exit('[-] Output file {0} already exists. '.
-                           format(output_file) +
+                           format(options['output']) +
                            'Use -c (clobber) to overwrite',
                            result)
-            os.remove(output_file)
-            verboseprint('[+] Overwriting {0}'.format(output_file))
+            os.remove(options['output'])
     except OSError as exception:
         print_exit('[-] Could not remove/overwrite file {0} ({1})'.
-                   format(output_file, exception.strerror), result)
-    result = (to_fo(saxon_binary, input_file, xsl_file, fop_output) and
-              to_PDF(fop_binary, fop_config, fop_output, output_file))
+                   format(options['output'], exception.strerror), result)
+    result = (to_fo(options) and
+              to_PDF(options))
     if result == 0:
-        print('[+] Succesfully build, output at {0}'.format(output_file))
+        print('[+] Succesfully build, output at {0}'.
+              format(options['output']))
     else:
         print_exit('[-] Unsuccessful (error {0})'.format(result), result)
 
