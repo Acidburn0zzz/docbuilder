47b1056a97c3ecf9fa268b6d50f6164d4e39043c
Peter Mosmans <support@go-forward.net>
2016-04-11 14:11:26 +1000

    pylint beautification

--- docbuilder.py
+++ docbuilder.py
@@ -12,6 +12,7 @@ the Free Software Foundation, either version 3 of the License, or
 
 from __future__ import absolute_import
 from __future__ import print_function
+
 import argparse
 import os
 import subprocess
